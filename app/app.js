a4ade60eea8c14d478ea78b924000e5f47294536
jmanlapid <jmanlapid@gmail.com>
2016-08-18 09:15:07 -0400

    ui-route template url
    Tempalte urls with the ui-router are giving me trouble. Sticking with master where i just define templates in index.html. In this branch use http-server npm package to serve the files.

--- app/app.js
+++ app/app.js
@@ -3,35 +3,24 @@
 angular.module('app', ['ngRoute', 'ui.router'])
 
 .config(function($routeProvider, $stateProvider) {
-  // $routeProvider.otherwise('/page1');
 
   $stateProvider.state(
     "page1", {
-      controller: 'Page1Ctrl'
-      template:[
-      '<div class="container">',
-        '<div class="row">',
-          '<h1>Video Splicer - Page ONE Template</h1>',
-        '<button></button>',
-        '</div>',
-        '<div class="row">',
-          '<div class="col-md-2">',
-            '<add></add>',
-            '<list></list>',
-          '</div>',
-          '<div class="col-md-8">',
-            '<player></player>',
-          '</div>',
-        '</div>',
-      '</div>'
-      ].join('')
+      url: '/page1',
+      controller: 'Page1Ctrl',
+      templateUrl: '/app/components/page1/page1.html'
+    },
+    "page2", {
+      url: '/page2',
+      controller: 'Page2Ctrl',
+      templateUrl: '/app/components/page2/page2.html'
     })
 
+  $routeProvider.otherwise('/page2');
 })
 
 .run(function($state) {
-  console.log('i am running');
-  $state.go('page1');
+  $state.go('page2')
 })
 
 .controller('AppCtrl', function() {
--- app/components/page1/page1.js
+++ app/components/page1/page1.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('app')
-  .controller('Page1Ctrl', function($scope) {
-
+  .controller('Page1Ctrl', function($scope, $state) {
+    angular.extend($scope, {
+      state: $state
+    });
   })
